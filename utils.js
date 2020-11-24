d4791100705a61337414ea18cfc4b74710369eba
stas-anikin <stas.terrace@gmail.com>
2020-11-24 13:38:53 -0800

    added subtract function

--- utils.js
+++ utils.js
@@ -1,3 +1,12 @@
 function product(num1, num2) {
     return num1*num2
 }
+function divide(num1, num2) {
+    return num1/num2
+}
+function power(num1, num2) {
+    return num1**num2
+}
+function subtract(num1, num2) {
+    return num1-num2
+}
